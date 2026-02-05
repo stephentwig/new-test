90f09ce89ac52104edc4d48d5f4a8eb3baa440b3
Stephen <step.twig@gmail.com>
2026-02-05 16:51:11 +0000

    save the OpenCV C++ Changes with image to file

---
+++ firstopencv.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+using namespace cv;
+
+int main() {
+    Mat img = Mat::zeros(500, 500, CV_8UC3);
+
+    rectangle(img, Point(50, 50), Point(200, 200), Scalar(0, 255, 0), 3);
+    circle(img, Point(300, 300), 50, Scalar(255, 0, 0), -1);
+    putText(img, "OpenCV", Point(100, 450),
+        FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2);
+
+    std::string out = "shapes.png";
+    if (imwrite(out, img)) {
+        std::cout << "Saved image to " << out << std::endl;
+        return 0;
+    } else {
+        std::cerr << "Failed to save image to " << out << std::endl;
+        return 1;
+    }
+}
